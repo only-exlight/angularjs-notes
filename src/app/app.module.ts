bedbb96d12f3502c8c9d2ac5555808101385cbfc
Kernichnyy Andrey <only.exlight@gmail.com>
2020-01-26 13:46:35 +0700

    Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { NotesPage } from './pages/notes/notes.page';
 import { MenuComponent } from './components/menu/menu.component';
 import { NoteList } from './components/note-list/note-list.component';
 import { StorageService } from './services/storage.service';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { ColorsComponent } from './components/colors/colors.component';
 
 module('app', ['ngRoute'])
   .component('appNote', Note)
   .component('appMenu', MenuComponent)
   .component('appNoteList', NoteList)
+  .component('appCreateNote', CreateNoteComponent)
+  .component('appColors', ColorsComponent)
   .service('noteService', NoteService)
   .service('storageService', StorageService)
   .config(($routeProvider: any) => {
--- src/app/pages/add-note/add-note.page.ts
+++ src/app/pages/add-note/add-note.page.ts
@@ -1,8 +1,19 @@
 import { IComponentOptions } from 'angular';
+import { NoteModel } from 'src/app/models';
+import { NoteService } from 'src/app/services/note.service';
 
-class AddNote {}
+class AddNote {
+  public note: NoteModel = new NoteModel();
+
+  constructor(private noteService: NoteService) {}
+
+  public saveNote() {
+    this.noteService.addNote(this.note);
+  }
+}
 
 export const AddNotePage: IComponentOptions = {
   template: require('./add-note.page.html'),
   controller: AddNote,
+  controllerAs: '$ctrl',
 };
