35ba752edd34bcafc0070285cdc5322156c4c332
Kernichnyy Andrey <only.exlight@gmail.com>
2020-01-26 12:12:18 +0700

    Update menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,15 @@
 import { IComponentOptions } from 'angular';
 
-class Menu {}
+class Menu {
+  public title: string;
+
+  public $onInit() {}
+}
 
 export const MenuComponent: IComponentOptions = {
   controller: Menu,
   template: require('./menu.component.html'),
+  bindings: {
+    title: '<',
+  },
 };
